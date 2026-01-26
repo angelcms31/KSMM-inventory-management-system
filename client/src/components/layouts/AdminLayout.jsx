ac65a0c6aa52aaca44590171ac31ad7ad0d48f81
AmiereMiranda <mremiranda2022@plm.edu.ph>
2026-01-27 01:44:42 +0800

    Update AdminLayout.jsx

--- client/src/components/layouts/AdminLayout.jsx
+++ client/src/components/layouts/AdminLayout.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import AdminSidebar from "../sidebars/AdminSidebar";
+import AdminFinanceRightSidebar from "../sidebars/AdminFinanceRightSideBar";
 
 export default function AdminLayout() {
   return (
-    <div className="flex min-h-screen w-screen bg-gray-50">
+    <div className="flex h-screen w-screen bg-gray-50 overflow-hidden">
       <AdminSidebar /> 
 
-      <div className="flex-1 flex flex-col">
-        
+      <div className="flex-1 flex flex-col overflow-y-auto">
         <main className="p-8 lg:p-12">
           <Outlet />
         </main>
       </div>
+
+      <AdminFinanceRightSidebar />
     </div>
   );
 }
